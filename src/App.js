40b368d34ec551949e086a86e4278cb3473aae4b
rohamm <rohambadi547@gmail.com>
2022-08-12 07:47:06 -0700

    optimize App.js

--- src/App.js
+++ src/App.js
@@ -3,27 +3,19 @@ import './App.css';
 import MyGif from './my.gif';
 function App() {
 
-    let mynumber = Math.floor(Math.random()*10)+1;
-    if(mynumber===7){
+    let mynumber = Math.floor(Math.random() * 10) + 1;
+    let myGifContainer = undefined;
 
-        return(
-            <div>
-                <h1>React Conditional Rendering</h1>
-                <h4>Number:{mynumber}</h4>
-                <img id='gif' src={MyGif} alt="MY Gif" />
-            </div>
-        )
-
-
-    }else{
-        return(
-            <div>
-                <h1>React Conditional Rendering</h1>
-                <h4>Number:{mynumber}</h4>
-            </div>
-        )
-    }
-
+    return(
+        <div>
+            <h1>React Conditional Rendering</h1>
+            <h4>Number:{mynumber}</h4>
+            {mynumber === 7 ?
+                <img id="gif" src={MyGif} alt="My Gif"/>
+                : ''
+            }
+        </div>
+    )
 
 }
 
